7c69064984f23cce4c9ca0b59314522a7c13ffd6
Jesus Manuel Lara <jmlr231201@gmail.com>
2024-02-26 03:38:10 -0600

    Hero Banner modificado resposivo

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,21 @@
-import Button from '../components/atoms/Button/Button';
+import Button from "../components/atoms/Button/Button";
+import HeroBanner from "../components/molecules/HeroBanner/HeroBanner";
+
+const HERO_BANNER_DATA={
+  img:"/image/FondoH.jpg", 
+  imgIglesia:"/image/iglesia.png",
+  title: "Bienvenido a EcoSol un punto de contacto para miembros de la comunidad",
+  description:"En EcoSol estamos comprometidos a brindarte las herramientas suficientes para que puedas establecer contacti con diferentes miembros de la comunidad parroquial  y logres intercambiar productos que sean de tu interes. Logrando que se fomenten las relaciones entre la comunidad."
+}
+
 
 export default function Home() {
-   return (
-      <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-         <Button variant='secondary' border='none' className=''>
-            Hola a todos
-         </Button>
-      </main>
-   );
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+       {/* <div className="z-20">jgmgjmfhjmnfgfh</div> Procurar que se vea este div*/}
+      <HeroBanner {...HERO_BANNER_DATA}/>
+      
+      
+    </main>
+  );
 }
